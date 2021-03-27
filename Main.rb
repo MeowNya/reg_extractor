e1d7ef05ba6141bc2aacdb7bdba0be6674c581c0
MeowNya <Evina666@gmail.com>
2021-03-27 14:11:30 +0500

    Finished Script

--- Main.rb
+++ Main.rb
@@ -1,6 +1,14 @@
 #! /usr/bin/env ruby
 
-
 __author__ = 'MeowNya'
 
+require 'open-uri'
 
+
+url = 'https://ru.wikipedia.org/wiki/Assassin%E2%80%99s_Creed_Chronicles'
+html = URI.open(url).read
+
+pattern = /2\d{3}/
+numbers = html.scan pattern
+numbers = numbers.uniq.sort
+puts numbers.join(', ')
